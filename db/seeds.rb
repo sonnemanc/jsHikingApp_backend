085ea87d2c3ba20ddc53f95e6e9206e9258120f1
Corban <sonnemanc@gmail.com>
2022-02-24 13:48:06 -0800

    hike, comment model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hike.delete_all
+Comment.delete_all
+
+locations = {
+    "..name.." => {
+        :difficulty => x,
+        :distance => x,
+        :best_season => x,
+        :trail_use => x,
+        :notes => x
+    },
+}
