5c8e0ae560e8364b861e69ba7f47211d3b07134b
Corban <sonnemanc@gmail.com>
2022-02-25 11:53:32 -0800

    jsonified hikes with comments.

--- app/controllers/api/v1/hikes_controller.rb
+++ app/controllers/api/v1/hikes_controller.rb
@@ -1,3 +1,17 @@
 class Api::V1::HikesController < ApplicationController
 
+  def index
+    hikes = Hike.all
+    render json: hikes, include: [:comments]
+  end
+
+  #def create
+    #hikes = Hike.new(hike_params) 
+  #end
+
+  private
+
+  def hike_params
+    
+  end
 end
